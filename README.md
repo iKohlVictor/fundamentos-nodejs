aac970801b306386fd02603a7c0f16730bc4e419
ian-kohl <ianvictor181099@gmail.com>
2022-02-05 19:13:07 -0300

    feat: fundamentals

---
+++ README.md
@@ -0,0 +1,29 @@
+Curso de nodejs das Trilhas do Ignite da Rocketseat.
+
+### Fundamentos Básicos NodeJS.
+
+# Utilizando express como framework para auxiliar na construção das rotas #
+
+ *npm install Express*
+
+ # Utilizando nodemon para deixar a aplicação rodando sem a necessidade de reiniciar a cada alteração #
+
+ *npm install -g nodemon*
+ *npm install --save-dev nodemon*
+
+ - Para configurar o nodemon no *package.json* na parte de *scripts* insira: 
+    "dev":"nodemon src/index.js"
+
+
+
+1 - Tipos de requisição
+* GET - Buscar uma informação dentro do servidor.
+* POST - Inserir uma informação no servidor.
+* PUT - Alterar uma informação no servidor.
+* PATCH - Alterar uma informação específica.
+* DELETE - Deletar uma informação no servidor.
+
+2 - Tipos de parâmetros
+* Route Params => Identificar um recurso editar/deletar/buscar.
+* Query Params => Paginação/Filtro
+* Body Params => Os objetos inserção/alteração (JSON)
--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@ const express = require("express");
 
 const app = express();
 
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log("query =>", query);
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log("body =>", body);
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  const params = request.params;
+  console.log("params =>", params);
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
+
 //localhost:3031
-app.listen();
+app.listen(3333);
